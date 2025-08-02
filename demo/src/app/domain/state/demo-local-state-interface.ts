6a6af885cee52db2531e0b1ce99749b16db76e1a
AtsuyaShimizu <atsuya252172@gmail.com>
2025-08-03 07:56:52 +0900

    feat: control execute and selectors with signals

--- demo/src/app/domain/state/demo-local-state-interface.ts
+++ demo/src/app/domain/state/demo-local-state-interface.ts
@@ -3,4 +3,7 @@ import { Signal } from "@angular/core";
 export interface DemoLocalState {
   readonly isEnableComplete: Signal<boolean>;
   readonly isEnableCancel: Signal<boolean>;
+  readonly isEnableExecute: Signal<boolean>;
+  readonly isEnableSelectUser: Signal<boolean>;
+  readonly isEnableSelectWork: Signal<boolean>;
 }
--- demo/src/app/domain/state/local/demo-local-B.state.ts
+++ demo/src/app/domain/state/local/demo-local-B.state.ts
@@ -9,11 +9,23 @@ export class DemoLocalStateB implements DemoLocalState {
 
   private _isEnableComplete = computed(() => this.globalState.progress() >= 7);
   private _isEnableCancel   = computed(() => this.globalState.progress() < 7);
+  private _isEnableExecute  = computed(() => this.globalState.progress() < 7);
+  private _isEnableSelectUser = computed(() => false);
+  private _isEnableSelectWork = computed(() => false);
 
   get isEnableComplete(): Signal<boolean> {
     return this._isEnableComplete;
   }
   get isEnableCancel(): Signal<boolean> {
     return this._isEnableCancel;
+  }
+  get isEnableExecute(): Signal<boolean> {
+    return this._isEnableExecute;
+  }
+  get isEnableSelectUser(): Signal<boolean> {
+    return this._isEnableSelectUser;
+  }
+  get isEnableSelectWork(): Signal<boolean> {
+    return this._isEnableSelectWork;
   }
 }
