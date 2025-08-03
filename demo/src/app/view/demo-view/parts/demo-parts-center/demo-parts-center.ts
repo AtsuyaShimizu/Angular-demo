d15018d3366ab3d32b0c6ab9aad2f9854c82a8d6
AtsuyaShimizu <atsuya252172@gmail.com>
2025-08-03 19:31:39 +0900

    refactor: move dialog to demo view

--- demo/src/app/view/demo-view/parts/demo-parts-center/demo-parts-center.ts
+++ demo/src/app/view/demo-view/parts/demo-parts-center/demo-parts-center.ts
@@ -1,12 +1,10 @@
 import { Component, EventEmitter, Input, Output, Signal, computed, signal } from '@angular/core';
-import { DemoPartsDialog } from '../demo-parts-dialog/demo-parts-dialog';
 
 @Component({
   selector: 'app-demo-parts-center',
   standalone: true,
   templateUrl: './demo-parts-center.html',
-  styleUrls: ['./demo-parts-center.scss'],
-  imports: [DemoPartsDialog]
+  styleUrls: ['./demo-parts-center.scss']
 })
 export class DemoPartsCenter {
   @Input() globalState: { workKind: Signal<string>; progress: Signal<number> } = {
@@ -17,27 +15,15 @@ export class DemoPartsCenter {
     isEnableComplete: Signal<boolean>;
     isEnableCancel: Signal<boolean>;
     isEnableExecute: Signal<boolean>;
-    isVisibleDialog: Signal<boolean>;
-    isEnablePlus: Signal<boolean>;
-    isEnableMinus: Signal<boolean>;
-    isEnableDecide: Signal<boolean>;
-    isEnableBack: Signal<boolean>;
   } = {
     isEnableComplete: signal(false),
     isEnableCancel: signal(false),
-    isEnableExecute: signal(false),
-    isVisibleDialog: signal(false),
-    isEnablePlus: signal(true),
-    isEnableMinus: signal(true),
-    isEnableDecide: signal(true),
-    isEnableBack: signal(true)
+    isEnableExecute: signal(false)
   };
 
   @Output() click_complete_event = new EventEmitter<void>();
   @Output() click_cancel_event   = new EventEmitter<void>();
   @Output() click_execute_event  = new EventEmitter<void>();
-  @Output() click_decide_event   = new EventEmitter<number>();
-  @Output() click_back_event     = new EventEmitter<void>();
 
   message: Signal<string> = computed(() =>
     `現在実行中の作業は${this.globalState?.workKind()}です。`
